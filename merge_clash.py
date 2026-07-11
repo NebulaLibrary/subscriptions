44ad99483f9820337cd0e17b75fbc49d26c3ba43
Nebula <142315441+NebulaLibrary@users.noreply.github.com>
2026-07-12 00:14:52 +0330

    Update merge_clash.py

--- merge_clash.py
+++ merge_clash.py
@@ -6,31 +6,46 @@ import os
 url = "https://raw.githubusercontent.com/Au1rxx/free-vpn-subscriptions/refs/heads/main/output/clash.yaml"
 
 output = "clash/clash.yaml"
+temp = "clash/clash_temp.yaml"
 
 
 try:
+
     r = requests.get(url, timeout=30)
     r.raise_for_status()
 
     data = yaml.safe_load(r.text)
 
-    # تغییر اسم همه پروکسی ها
-    if "proxies" in data:
+
+    if "proxies" in data and len(data["proxies"]) > 0:
+
         for proxy in data["proxies"]:
             proxy["name"] = "@SystemNebula"
 
-    # ساخت پوشه clash اگر نبود
-    os.makedirs("clash", exist_ok=True)
 
-    with open(output, "w", encoding="utf-8") as f:
-        yaml.dump(
-            data,
-            f,
-            allow_unicode=True,
-            sort_keys=False
-        )
+        os.makedirs("clash", exist_ok=True)
 
-    print("Clash updated successfully")
+
+        with open(temp, "w", encoding="utf-8") as f:
+            yaml.dump(
+                data,
+                f,
+                allow_unicode=True,
+                sort_keys=False
+            )
+
+
+        os.replace(temp, output)
+
+        print("Clash updated:", len(data["proxies"]))
+
+
+    else:
+
+        print("Clash failed. Old file kept.")
+
 
 except Exception as e:
-    print("Error:", e)
+
+    print("Clash error:", e)
+    print("Old file kept.")
